8ce53a28c3a61b5d91cd350ee82ee0829b49f7a2
Bhuvaneshcmv <bhuvaneshcmv@gmail.com>
2017-08-22 23:02:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+    res.send('U have Requested for article one and it will be served shortly');
+});
+
+app.get('/article-two',function(req,res){
+    res.send('U have Requested for article 2 and it will be served shortly');
+});
+
+app.get('/article-three',function(req,res){
+    res.send('U have Requested for article 3 and it will be served shortly');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
